05e35445a5abc19e268ad85906b9e50edfd13427
andrei-gorbatch <76885880+andrei-gorbatch@users.noreply.github.com>
2023-11-05 22:40:55 +0000

    Ingest info from excel

--- src/simulation.py
+++ src/simulation.py
@@ -7,7 +7,36 @@ import random
 
 from classes import martial, blaster
 from utils import calculate_attack_damage, calculate_spell_damage, calculate_group_hp
-from config import monte_carlo_iterations, heroes, monsters
+from config import monte_carlo_iterations, data_path
+
+def ingest_creatures_from_excel():
+    # Function to ingest heroes and monsters information from excel spreadsheet
+    input_file = data_path/'character_info.xlsx'
+    heroes_df = pd.read_excel(input_file, sheet_name="Heroes")
+    heroes_df.name = 'heroes'
+    monsters_df = pd.read_excel(input_file, sheet_name="Monsters")
+    monsters_df.name = 'monsters'
+
+    dfs = [heroes_df, monsters_df]
+    characters_dict=dict()
+    for df in dfs:
+        characters = []
+        for index, row in df.iterrows():
+            saves = {"str": row['str_save'], "dex": row['dex_save'], "con": row['con_save'], "wis": row['wis_save'], "cha": row['cha_save'], "int": row['int_save']}
+            if row['Type']=="Martial":
+                #TODO: add error handling for missing values, wrong formats, etc.
+                characters.append(martial(name=row['Name'], hp=row['HP'], ac=row['AC'], saves=saves, initiative_bonus=row['initiative_bonus'],
+                                    healer = row['healer'], number_of_attacks=row['number_of_attacks'], attack_bonus=row['attack_bonus'], attack_damage=row['attack_damage']))
+            elif row['Type']=="Blaster":
+                characters.append(blaster(name=row['Name'], hp=row['HP'], ac=row['AC'], saves=saves, initiative_bonus=row['initiative_bonus'],
+                                    healer = row['healer'], number_of_targets=row['number_of_targets'], spell_save_dc=row['spell_save_dc'],
+                                    targeted_save=row['targeted_save'], attack_damage=row['attack_damage']))
+            else:
+                print(f"row {index} contains unknown type {row['Type']}, please check.")
+
+        characters_dict[df.name] = characters
+
+    return characters_dict #['heroes'], characters_dict['monsters']
 
 def choose_heal_or_attack_target(character, heroes, monsters, number_of_targets=1):
     # Function that chooses whether to heal an ally or attack an enemy, and selects the target
@@ -53,8 +82,10 @@ def choose_heal_or_attack_target(character, heroes, monsters, number_of_targets=
     return target_type, targets
 
 
-def initialize_combat():
+def initialize_combat(characters_dict):
     # Function to reset the combat
+    heroes = characters_dict['heroes']
+    monsters = characters_dict['monsters']
     all_creatures = heroes + monsters
 
     initiative_dict = dict()
@@ -68,7 +99,7 @@ def initialize_combat():
     heroes_hp = calculate_group_hp(heroes)
     rounds=0
 
-    return heroes, monsters, all_creatures, initiative_dict, monsters_hp, heroes_hp, rounds
+    return heroes, monsters, initiative_dict, monsters_hp, heroes_hp, rounds
 
 def action(character, heroes, monsters):
     # Function that performs one action for the character
@@ -91,10 +122,10 @@ def action(character, heroes, monsters):
             for target in targets:
                 target.take_damage_or_status(best_action)
 
-def run_one_combat():
+def run_one_combat(characters_dict):
     # Function to run one combat
 
-    heroes, monsters, all_creatures, initiative_dict, monsters_hp, heroes_hp, rounds = initialize_combat()
+    heroes, monsters, initiative_dict, monsters_hp, heroes_hp, rounds = initialize_combat(characters_dict)
 
     while (monsters_hp>0 and heroes_hp>0):
         rounds=rounds+1
@@ -107,11 +138,11 @@ def run_one_combat():
     return rounds, heroes_hp, monsters_hp
 
 
-def monte_carlo() -> pd.DataFrame:
+def monte_carlo(characters_dict) -> pd.DataFrame:
     # Run combat for number of iterations defined in config and save results in a df for futher analysis
     data = []
     for i in range(0, monte_carlo_iterations):
-        rounds, heroes_hp, monsters_hp = run_one_combat()
+        rounds, heroes_hp, monsters_hp = run_one_combat(characters_dict)
         data.append([rounds, heroes_hp, monsters_hp])
     combats_df = pd.DataFrame(columns=['rounds','heroes_hp', 'monsters_hp'], data = data)
     combats_df['TPK'] = np.where((combats_df['heroes_hp']==0), True, False)
@@ -119,7 +150,8 @@ def monte_carlo() -> pd.DataFrame:
     return combats_df
 
 def main():
-    combats_df = monte_carlo()
+    characters_dict = ingest_creatures_from_excel()
+    combats_df = monte_carlo(characters_dict)
     print(f"Ran {monte_carlo_iterations} combats, {combats_df['TPK'].sum()} of them are TPK!")
 
 if __name__ == "__main__":
